6c06f8a47f04dbbcef4fc9501df4a46b65dcaa3b
sobhanism <sobhani.sm@gmail.com>
2025-10-02 22:03:28 +0330

    fix: update mouse tracker

--- src/components/MouseTrackerProvide.vue
+++ src/components/MouseTrackerProvide.vue
@@ -4,15 +4,15 @@
   <script setup>
   import { onMounted, onUnmounted, provide, ref } from 'vue';
   const mouseTracker=ref({x:0,y:0})
-  const updateMousePosition = (event) => {
+  const updateMouseTracker = (event) => {
     mouseTracker.value.x=event.clientX
     mouseTracker.value.y=event.clientY
   }
   onMounted(() => {
-    window.addEventListener('mousemove', updateMousePosition)
+    window.addEventListener('mousemove', updateMouseTracker)
   })
   onUnmounted(() => {
-    window.removeEventListener('mousemove', updateMousePosition)
+    window.removeEventListener('mousemove', updateMouseTracker)
   })
   provide('mouseTracker',mouseTracker)
   </script>
